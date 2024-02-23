9d75aa1afbf6a5236c5d546e0b3f515e99634479
范传奇 <83123095@qq.com>
2024-02-23 10:20:36 +0800

    @Autowired注解2

--- _03spring/src/main/java/cn/tedu/spring/bean/UserSerive.java
+++ _03spring/src/main/java/cn/tedu/spring/bean/UserSerive.java
@@ -7,9 +7,23 @@ import org.springframework.stereotype.Service;
 public class UserSerive {
     /*
         1.@Autowired注解:注入对象或者接口类型的数据;
+        2.添加位置
+          2.1 添加在属性上;
+          2.2 添加在set方法上;
+          2.3 添加在构造方法上;
      */
-    @Autowired
+    //@Autowired
     private UserDao userDao;
+
+    //@Autowired
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Autowired
+    public UserSerive(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     @Override
     public String toString() {
