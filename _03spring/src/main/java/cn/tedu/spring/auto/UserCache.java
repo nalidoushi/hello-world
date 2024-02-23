687250af942cfc3258725bc495d42550d304ae96
范传奇 <83123095@qq.com>
2024-02-23 10:51:42 +0800

    @Autowired中required参数

--- _03spring/src/main/java/cn/tedu/spring/auto/UserCache.java
+++ _03spring/src/main/java/cn/tedu/spring/auto/UserCache.java
@@ -5,9 +5,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserCache {
-    @Autowired
+    /*
+        required参数:
+        * true[默认]:装配失败会抛出异常;
+        * false:装配失败也不会抛出异常,一般不使用;
+     */
+    @Autowired(required = true)
     private Cache cache;
 }
+
+
+
+
+
+
 
 
 
